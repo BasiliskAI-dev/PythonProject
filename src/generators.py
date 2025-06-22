1996811113d16f68230f4d0ca8802249202faed5
Ilya Vasilenko <ilyavasilenko2000@gmail.com>
2025-06-22 18:51:05 +0500

    added generators with functions

--- src/generators.py
+++ src/generators.py
@@ -1,83 +1,29 @@
+from typing import Any, Generator
 
 
-def filter_by_currency(transactions: list, valute: str):
-    gener = (x for x in transactions if x["operationAmount"]["currency"]["name"] == valute)
+def filter_by_currency(transactions: list, valute: str) -> Generator[Any, Any, None]:
+    """Возвращает словарь из списка transactions по коду valute"""
+    gen = (x for x in transactions if x["operationAmount"]["currency"]["code"] == valute)
+    return gen
 
 
+def transaction_descriptions(transactions: list) -> Generator[Any, Any, None]:
+    """Возвращает описание переводов из списка transactions"""
+    gen = (x["description"] for x in transactions if "description" in x)
+    return gen
 
 
+def card_number_generator(start: int, stop: int) -> Generator[Any, Any, None]:
+    """Возвращает номер карты в диапазоне от start до stop"""
+    default_number = 10000000000000000 + start
+    for x in range(start, stop):
+        new_number = ""
+        str_default_number = str(default_number)[1:]
+        for _ in range(0, len(str_default_number), 4):
+            new_number += str_default_number[_ : _ + 4] + " "
+        yield new_number
+        default_number += 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-transactions = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    },
-    {
-        "id": 895315941,
-        "state": "EXECUTED",
-        "date": "2018-08-19T04:27:37.904916",
-        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод с карты на карту",
-        "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229",
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657",
-    },
-]
-
-
-
-print(next(filter_by_currency(transactions, "RUB")))
-print(next(filter_by_currency(transactions, "RUB")))
-print(next(filter_by_currency(transactions, "RUB")))
+for card_number in card_number_generator(1234123412341234, 1234123412349999):
+    print(card_number)
